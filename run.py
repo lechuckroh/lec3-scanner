a174953ba43b4507802f6d019a1f9d29f9ccd839
Lechuck Roh <member@lechuck2011.cafe24.com>
2017-01-16 22:26:48 +0900

    process subdirs

--- run.py
+++ run.py
@@ -1,38 +1,58 @@
 import argparse
 
 import sys
+import zipfile
 
 from scanner.file import *
+from scanner.info import *
 
 
-def process_dir(path):
+def process_dir(dirpath):
     txtfiles = []
     zipfiles = []
     subdirs = []
-    print("processing : ", path)
-    for f in os.listdir(path):
-        if os.path.isdir(f):
-            subdirs.append(f)
+    print("processing : ", dirpath)
+    for f in os.listdir(dirpath):
+        filepath = os.path.join(dirpath, f)
+        if os.path.isdir(filepath):
+            subdirs.append(filepath)
         else:
             ext = get_extension(f)
             if ext == '.txt':
-                txtfiles.append(f)
+                txtfiles.append(filepath)
             elif ext == '.zip':
-                zipfiles.append(f)
+                zipfiles.append(filepath)
 
     for subdir in subdirs:
         process_dir(subdir)
+
+    bookinfo = None
     for f in txtfiles:
-        print("text file : ", f)
+        info = read_info_file(f)
+        if not bookinfo:
+            bookinfo = info
+        else:
+            # TODO: merge info to bookinfo
+            pass
+
+    if bookinfo:
+        print("%s : %s" % (dirpath, bookinfo.isbn))
+
     for f in zipfiles:
-        print("zip file : ", f)
+        with zipfile.ZipFile(f) as zf:
+            filenames = zf.namelist()
+            count = len(filenames)
+            if bookinfo:
+                if bookinfo.scan_pages != count:
+                    print("scan pages mismatch. %d <> %d" %
+                          (bookinfo.scan_pages, count))
 
 
 parser = argparse.ArgumentParser(description='Scan bookscan images')
 parser.add_argument('--src', help='source directory', default='./')
 args = parser.parse_args()
 
-src_dir = os.path.abspath(args.src)
+src_dir = os.path.abspath(os.path.expanduser(args.src))
 
 # check source directory exists
 if not os.path.exists(src_dir):
